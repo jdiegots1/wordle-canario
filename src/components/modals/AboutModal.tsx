400e47dd5ba803f371aead4e5c6479c7398175c4
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-06 14:58:30 +0100

    Add files via upload

---
+++ src/components/modals/AboutModal.tsx
@@ -0,0 +1,22 @@
+import { BaseModal } from './BaseModal'
+
+type Props = {
+  isOpen: boolean
+  handleClose: () => void
+}
+
+export const AboutModal = ({ isOpen, handleClose }: Props) => {
+  return (
+    <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Por @JDiegoTejeraS - A partir del código abierto -{' '}
+        <a
+          href="https://github.com/hannahcode/GAME"
+          className="underline font-bold"
+        >
+          encuentra el código aquí
+        </a>{' '}
+      </p>
+    </BaseModal>
+  )
+}
