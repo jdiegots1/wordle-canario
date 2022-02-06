e9dcc519dab2b42d43d8882991639dad73a184d1
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-06 14:54:12 +0100

    Add files via upload

---
+++ src/constants/strings.ts
@@ -0,0 +1,19 @@
+export const GAME_TITLE = 'GAME'
+
+export const WIN_MESSAGES = ['¡De locos!', 'Awesome', 'Well done!']
+export const GAME_COPIED_MESSAGE = 'Copiado en el portapapeles'
+export const ABOUT_GAME_MESSAGE = 'Sobre este juego'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
+export const WORD_NOT_FOUND_MESSAGE = 'La palabra está en San Borondón (no existe)'
+export const CORRECT_WORD_MESSAGE = (solution: string) =>
+  `The word was ${solution}`
+export const ENTER_TEXT = 'Palante'
+export const DELETE_TEXT = 'Borrar'
+export const STATISTICS_TITLE = 'Estadísticas'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribución de los aciertos'
+export const NEW_WORD_TEXT = 'Nueva palabra'
+export const SHARE_TEXT = 'Compartir'
+export const TOTAL_TRIES_TEXT = 'Intentos totales'
+export const SUCCESS_RATE_TEXT = 'Ratio de acierto'
+export const CURRENT_STREAK_TEXT = 'Current streak'
+export const BEST_STREAK_TEXT = 'Best streak'
