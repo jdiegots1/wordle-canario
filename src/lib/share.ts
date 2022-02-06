a1056cec726c00d07a39998af4b88d84c0a68b70
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-06 21:50:25 +0100

    Update share.ts

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -29,3 +29,5 @@ export const generateEmojiGrid = (guesses: string[]) => {
     })
     .join('\n')
 }
+export const shareStatus = (guesses: string[], lost: boolean) => {
+  navigator.clipboard.writeText(href="https://wordle-canario.vercel.app/")
