aed25529099f4a4bf1a048768d7ccfb71e335da0
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-08 00:19:04 +0100

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -7,7 +7,7 @@ export const ABOUT_GAME_MESSAGE = 'Sobre este juego'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
 export const WORD_NOT_FOUND_MESSAGE = ['A otro, que sea más primo, vale, pero a mí no. Escribe una palabra que exista, hazme el favor.', 'Métele mano a eso, que esa palabra no existe']
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `El conejo te enriscó la perra: la palabra era ${solution}`
+  `El conejo te enriscó la perra: la palabra era ${solution}` if ${solution} = folia ('La folía es la música tradicional y característica de nuestras Islas. Una parranda con guitarras y timples = folía perfecta.') 
 export const ENTER_TEXT = 'Palante'
 export const DELETE_TEXT = 'Patrás'
 export const STATISTICS_TITLE = 'Estadísticas'
