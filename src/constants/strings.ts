55324b252338070e6fadc50b94a361a52187b963
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-07 23:09:47 +0100

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -6,9 +6,9 @@ export const ABOUT_GAME_MESSAGE = 'Sobre este juego'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
 export const WORD_NOT_FOUND_MESSAGE = ['A otro, que sea más primo, vale, pero a mí no. Escribe una palabra que exista, hazme el favor.', 'Métele mano a eso, que esa palabra no existe']
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `La palabra era ${solution}`
+  `El conejo te enriscó la perra: la palabra era ${solution}`
 export const ENTER_TEXT = 'Palante'
-export const DELETE_TEXT = 'Borrar'
+export const DELETE_TEXT = 'Patrás'
 export const STATISTICS_TITLE = 'Estadísticas'
 export const GUESS_DISTRIBUTION_TEXT = 'Distribución de los aciertos'
 export const NEW_WORD_TEXT = 'Siguiente palabra'
