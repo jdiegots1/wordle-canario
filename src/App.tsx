0ec23c872f6aaa1153c77c274824f98352de9934
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-09 22:25:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import { InfoModal } from './components/modals/InfoModal'
 import { StatsModal } from './components/modals/StatsModal'
 import {
   GAME_TITLE,
-  GAME_SUBTITLE,
   WIN_MESSAGES,
   GAME_COPIED_MESSAGE,
   ABOUT_GAME_MESSAGE,
