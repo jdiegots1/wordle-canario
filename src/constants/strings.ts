5d11cb132f07e40e3e8bd59f844f7778fc27a31d
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-09 23:54:15 +0100

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,13 +1,13 @@
 export const GAME_TITLE = 'WORDLE CANARIO'
 export const GAME_SUBTITLE = 'Para palicar con fundamento'
 
-export const WIN_MESSAGES = ['De locos', 'Estás hecho un puntal', 'Creeeema', 'Chiquito jeito tienes pa esto, hermano 👌']
+export const WIN_MESSAGES = ['De locos <br/>El naife es un cuchillo', 'Estás hecho un puntal', 'Creeeema', 'Chiquito jeito tienes pa esto, hermano 👌']
 export const GAME_COPIED_MESSAGE = 'Copiado en el portapapeles'
 export const ABOUT_GAME_MESSAGE = 'Sobre este juego'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
 export const WORD_NOT_FOUND_MESSAGE = ['A otro, que sea más primo, vale, pero a mí no. Escribe una palabra que exista, hazme el favor.', 'Métele mano a eso, que esa palabra no existe', 'Venga, sí. Esa palabra 1, Las Palmas 2. Pon una palabra que exista, hermano']
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `El conejo te enriscó la perra: la palabra era ${solution}` 
+  `El conejo te enriscó la perra: la palabra era ${solution}`
 export const ENTER_TEXT = 'Palante'
 export const DELETE_TEXT = 'Patrás'
 export const STATISTICS_TITLE = 'Estadísticas'
