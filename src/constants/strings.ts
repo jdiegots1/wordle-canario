db52d07b4a1eafd4b5dea47c785fc182ddf2dfa4
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-17 00:05:24 +0100

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,13 +1,13 @@
 export const GAME_TITLE = 'WORDLE CANARIO'
 export const GAME_SUBTITLE = 'Para palicar con fundamento'
 
-export const WIN_MESSAGES = ['Lo mínimo que te mereces después de acertar es un anochecer en Las Canteras, un viajecito a La Palma para comer papas locas o ir a un buen guachinche en Tenerife. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico', 'Naa, me tienes enchochadísimo 😍. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico.', 'De locos. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico.', 'Estás hecho un puntal. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico.', 'Creeeema. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico.', 'Chiquito jeito tienes pa esto, hermano 👌. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico.', 'Lo mínimo que te mereces después de acertar es una buena verbena 🕺💃. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico.']
+export const WIN_MESSAGES = ['Lo mínimo que te mereces después de acertar es un anochecer en Las Canteras, un viajecito a La Palma para comer papas locas o ir a un buen guachinche en Tenerife', 'Naa, me tienes enchochadísimo 😍', 'Estás hecho un puntal', 'Creeeema', 'Chiquito jeito tienes pa esto, hermano 👌', 'Lo mínimo que te mereces después de acertar es una buena verbena 🕺💃']
 export const GAME_COPIED_MESSAGE = 'Copiado en el portapapeles'
 export const ABOUT_GAME_MESSAGE = 'Sobre el juego'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'No hay suficientes letras'
 export const WORD_NOT_FOUND_MESSAGE = ['Pon una palabra más que no exista y la isla se te queda chica, hermano. Corrige eso', 'A otro, que sea más primo, vale, pero a mí no. Escribe una palabra que exista, hazme el favor.', 'Métele mano a eso, que esa palabra no existe', 'Venga, sí. Esa palabra 1, Las Palmas 2. Pon una palabra que exista, hermano', 'Venga, estás bonito tú. Pon una palabra que exista, hazme el favor', ]
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `El conejo te enriscó la perra: la palabra era ${solution}. Tupir es cuando se atasca un conducto u orificio. O lo que te ocurre cuando te comes un tuno o higo pico`
+  `El conejo te enriscó la perra: la palabra era ${solution}`
 export const ENTER_TEXT = 'Palante'
 export const DELETE_TEXT = 'Patrás'
 export const STATISTICS_TITLE = 'Estadísticas'
