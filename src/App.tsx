b3e77115feed4c524416d5d58ef8b604e8c22d95
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-20 19:58:04 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -160,11 +160,12 @@ function App() {
   }
 
   return (
-    <div className="py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div className="text-black dark:text-white bg-white dark:bg-gray-900 py-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div className="flex w-80 mx-auto items-center mb-8 mt-12">
         <h1 className="text-xl ml-2.5 grow font-bold dark:text-white">
           {GAME_TITLE}
         </h1>
+        Para palicar con fundamento
         {isDarkMode ? (
           <SunIcon
             className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
