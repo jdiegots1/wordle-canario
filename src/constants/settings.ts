187ba5be9deadcaa8ff730aa288b86502071bab1
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-21 00:46:51 +0100

    Update settings.ts

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,2 +1,4 @@
 export const MAX_WORD_LENGTH = 5
 export const MAX_CHALLENGES = 6
+export const GIF_URL = (solution: string) =>
+  `https://github.com/jdiegots1/wordleprueba/blob/A-VER/src/constants/pepe.jpg`
