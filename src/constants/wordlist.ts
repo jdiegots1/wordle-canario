2871b2e927a4c10812a7fc60fa649964fa2d18e3
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-21 21:25:39 +0100

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -10,6 +10,11 @@ const DEFINITIONS: Definition[] = [
         definition: 'persona torpe, ruda, tarda en comprender.',
         example: 'En el último partido los jugadores de Las Palmas parecían unos tollos.'
     },
+    {
+        word: 'urrar',
+        definition: 'dar urridos: la voz característa de las reses vacunas o también puede ser un grito fuerte y desgarrado a causa de un dolor.',
+        example: 'El pibe empezó a urrar cuando su meñique golpeó el mueble.'
+    },
 ]
 
 let WORDS: string[] = []
