2eb98deaf589bcc3ded8b5083c547d9bbd186179
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-22 02:09:58 +0100

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,11 +6,6 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'tollo',
-        definition: 'persona torpe, ruda, tarda en comprender.',
-        example: 'En el último partido los jugadores de Las Palmas parecían unos tollos.'
-    },
-    {
         word: 'urrar',
         definition: 'dar urridos: la voz característa de las reses vacunas o también puede ser un grito fuerte y desgarrado a causa de un dolor.',
         example: 'El pibe empezó a urrar cuando su meñique golpeó el mueble.'
