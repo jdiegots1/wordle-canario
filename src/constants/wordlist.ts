83e2b76ea0f87fceaf063dda6b785e7ca8b75d75
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-23 09:30:45 +0100

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -9,7 +9,7 @@ const DEFINITIONS: Definition[] = [
         word: 'baifo',
         definition: 'es la cría de la cabra. Si “se te va el baifo” es que se te olvidó lo que ibas a hacer o ibas a decir',
 
-        example: 'Cuando lo oían llegar con el brazado de millo seco, los urridos se oían desde lejos.'
+        example: ''
     },
 ]
 
