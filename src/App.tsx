c04d0c9120bcc859ff8105735906cc8c6317e4c3
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-02-25 11:22:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -165,9 +165,9 @@ function App() {
           {GAME_TITLE}
         </h1>
         Para palicar con fundamento
-        <br><h5>
+        <br>
           Cada día, entre las 08:00 y 08:30, nueva palabra
-          </h5></br>
+        </br>
         {isDarkMode ? (
           <SunIcon
             className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
