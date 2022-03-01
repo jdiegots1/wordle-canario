d4a1e06d1319e64381c4bacdd9f8c0abfe2c5cbc
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-03-01 09:58:57 +0100

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,9 +6,9 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'sitar',
-        definition: 'Llamar a una persona emitiendo un sonido silbante corto y reiterado.',
-        example: 'Se ofendía si lo sitaban en lugar de llamarlo por su nombre.'
+        word: 'lucha',
+        definition: 'deporte tradicional de las Islas, consistente en un enfrentamiento entre dos rivales en el que procura cada uno de ellos vencer al otro tumbándolo o haciendo que toque el suelo con alguna parte del cuerpo distinta de las plantas de los pies. También se puede emplear la frase “no cojas lucha”, que significa que no te preocupes tanto, que hay que tomarse las cosas con calma y no ofuscarse con los problemas',
+        example: 'No cojas lucha, yo tampoco me sabía la palabra del Wordle canario de hoy.'
     },
 ]
 
