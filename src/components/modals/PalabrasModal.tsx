09c25debd0880ea644338f6fe1e181360fd0e1e8
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-03-02 17:50:35 +0100

    Update PalabrasModal.tsx

--- src/components/modals/PalabrasModal.tsx
+++ src/components/modals/PalabrasModal.tsx
@@ -11,7 +11,7 @@ export const PalabrasModal = ({ isOpen, handleClose }: Props) => {
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Mira el listado de palabras que aparecieron en días anteriores {' '}
         <a
-          href="./wordlecanariopalabras.html"
+          href="https://palabras-wordle-canario.vercel.app/"
           className="underline font-bold"
         >
           aquí
