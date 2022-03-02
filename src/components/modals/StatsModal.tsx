4fb20d3ee6bdaa893d5ccdf5ed1bd697eaa6b609
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-03-02 17:56:13 +0100

    Update StatsModal.tsx

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -98,6 +98,13 @@ export const StatsModal = ({
         </div>
       )}
       
+      <div>
+        Mira todas las palabras que ya salieron en días anteriores{' '}
+        <a href="www.palabras-wordle-canario.vercel.app" className="underline font-bold">
+          aquí
+        </a>{' '}
+      </div>
+      
       <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
         <br />
         {STATISTICS_TITLE}
