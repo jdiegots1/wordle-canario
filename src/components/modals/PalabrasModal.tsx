32b235ea1038a8106ea4dbb14cbe7120f8bf7f6b
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-03-02 17:33:40 +0100

    Update PalabrasModal.tsx

--- src/components/modals/PalabrasModal.tsx
+++ src/components/modals/PalabrasModal.tsx
@@ -9,9 +9,9 @@ export const PalabrasModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="Glosario" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Mira el listado de palabras que aparecieron en días anteriores -{' '}
+        Mira el listado de palabras que aparecieron en días anteriores {' '}
         <a
-          href="https://github.com/hannahcode/GAME"
+          href="wordlecanariopalabras.html"
           className="underline font-bold"
         >
           aquí
