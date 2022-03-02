3cc1ec926353c51f1756ef03aa568a82a7aea487
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-03-02 09:32:00 +0100

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,11 +6,6 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'lucha',
-        definition: 'deporte tradicional de las Islas, consistente en un enfrentamiento entre dos rivales en el que procura cada uno de ellos vencer al otro tumbándolo o haciendo que toque el suelo con alguna parte del cuerpo distinta de las plantas de los pies. También se puede emplear la frase “no cojas lucha”, que significa que no te preocupes tanto, que hay que tomarse las cosas con calma y no ofuscarse con los problemas',
-        example: 'No cojas lucha, yo tampoco me sabía la palabra del Wordle canario de hoy.'
-    },
-    {
         word: 'tolmo',
         definition: 'Piedra grande',
         example: 'Con las lluvias cayeron unos tolmos a la carretera que impidieron la circulación.'
