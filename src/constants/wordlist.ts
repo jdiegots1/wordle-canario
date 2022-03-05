3779e02b82b265861e6b4c4e1cf05fd489cffcad
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-03-05 10:33:35 +0100

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,9 +6,9 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'drago',
-        definition: 'Árbol de la familia de las dracaenáceas, que puede alcanzar entre 12 y 14 metros de altura. La savia, llamada por su color rojizo sangre de drago, es usada en medicina y en la elaboración de tintes y barnices. Es una especie endémica de la región macaronésica.',
-        example: ''
+        word: 'fleje',
+        definition: 'Gran cantidad de personas, animales o cosas.',
+        example: 'A la entrada del estadio había un fleje de policías.'
     },
 ]
 
