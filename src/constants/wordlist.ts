c459e25f1f211212bc7848cb97eb65e898c4c4f9
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-03-18 10:49:51 +0100

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,9 +6,9 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'mamar',
-        definition: 'Obtener provecho indebidamente a costa de otro, de empresas o de instituciones públicas o privadas; tomar para sí lo ajeno, birlar.',
-        example: 'La política canaria está llena de mamones'
+        word: 'fecho',
+        definition: 'Pasador, pestillo.',
+        example: ''
     },
 ]
 
