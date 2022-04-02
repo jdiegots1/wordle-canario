7fee562bfa22d43ebda4b73b9767162b813cba92
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-04-02 12:07:02 +0200

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,9 +6,9 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'sargo',
-        definition: 'Persona astuta, taimada',
-        example: 'Ten cuidado con esos gangocheros, que son unos sargos y te meten gato por liebre.'
+        word: 'picon',
+        definition: 'Puede ser lo dicho de ciertos condimentos, como la pimienta o la guindilla, y de las salsas que se hacen con ellos, que produce ardor en el paladar o arena volcánica.',
+        example: 'A estas pimientas hay que quitarles la granilla porque, si no, el mojo sale muy picón.'
     },
 ]
 
