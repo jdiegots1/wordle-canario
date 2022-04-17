b1702627b14369ec6f85df5cfb159e25515035de
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-04-17 23:59:58 +0200

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,9 +6,9 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'sable',
-        definition: 'Arena volcánica.',
-        example: ''
+        word: 'airon',
+        definition: 'Podemos referirnos a la corriente de aire súbita que puede producir efectos perjudiciales para la salud o a la jaqueca o malestar causado por una corriente de aire.',
+        example: 'Ella siempre decía que había que tener cuidado con las corriente porque podías coger un airón.'
     },
 ]
 
