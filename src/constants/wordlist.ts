681fcf55b3cb89dac16bfc2344cbd1a3205d3331
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-04-23 00:13:34 +0200

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -6,9 +6,9 @@ type Definition = {
 
 const DEFINITIONS: Definition[] = [
     {
-        word: 'sacho',
-        definition: 'Instrumento que consiste en una lámina de hierro en forma de media luna, con un cabo largo que encaja en un anillo situado bajo el borde de la parte superior del lado curvo, que se emplea para cavar la tierra.',
-        example: 'Tuvo que comprar otro sacho porque ese ya tenía las puntas romas.'
+        word: 'mareo',
+        definition: 'Persona que molesta o importuna a causa de su insistencia o monotonía.',
+        example: 'Llévate al mareo este de aquí, que me tiene la cabeza loca.'
     },
 ]
 
