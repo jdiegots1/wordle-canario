c5fcac738fd4a2857bb0dc8640224f28e144fe74
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2022-05-06 23:28:59 +0200

    Update Keyboard.tsx

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -98,7 +98,7 @@ export const Keyboard = ({
         ))}
       </div>
       <div className="flex justify-center">
-        <Key width={65.4} value="ELIMINAR" onClick={onClick}>
+        <Key width={65.4} value="PALANTE" onClick={onClick}>
           {DELETE_TEXT}
         </Key>
         {[
@@ -118,8 +118,8 @@ export const Keyboard = ({
             isRevealing={isRevealing}
           />
         ))}
-        <Key width={65.4} value="PALANTE" onClick={onClick}>
-          {ENTER_TEXT}
+        <Key width={65.4} value="ELIMINAR" onClick={onClick}>
+          {DELETE_TEXT}
         </Key>
       </div>
     </div>
