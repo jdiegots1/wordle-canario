4e70de018c95932637f557d0b9abda735b463e1e
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-23 20:54:31 +0000

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,32 +7,35 @@ type Props = {
   setIsInfoModalOpen: (value: boolean) => void
   setIsStatsModalOpen: (value: boolean) => void
   setIsSettingsModalOpen: (value: boolean) => void
-};
+}
 
-        <div className="flex w-40 mx-auto items-center mb-5 mt-8">
-        <img
-          src={WCLOGO}
-          alt=""
-        />
-        </div>
-        <div className="right-icons">
-          <ChartBarIcon
-            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
-            onClick={() => setIsStatsModalOpen(true)}
-          />
-          <div className="flex h-6 w-6 mx-auto">
-            <a title="Twitter"
-              href="https://twitter.com/wordlecanario"><img
-              src={twitter}
-              alt="" /></a></div>
-            
-          {/* <CogIcon
-            className="h-6 w-6 cursor-pointer dark:stroke-white"
-            onClick={() => setIsSettingsModalOpen(true)}
-          /> */}
-        </div>
+const Navbar = ({ setIsInfoModalOpen, setIsStatsModalOpen, setIsSettingsModalOpen }: Props) => {
+  return (
+    <div>
+      <div className="flex w-40 mx-auto items-center mb-5 mt-8">
+        <img src={WCLOGO} alt="" />
       </div>
-      <hr></hr>
+      <div className="right-icons">
+        <ChartBarIcon
+          className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+          onClick={() => setIsStatsModalOpen(true)}
+        />
+        <div className="flex h-6 w-6 mx-auto">
+          <a
+            title="Twitter"
+            href="https://twitter.com/wordlecanario"
+          >
+            <img src={twitter} alt="" />
+          </a>
+        </div>
+        {/* <CogIcon
+          className="h-6 w-6 cursor-pointer dark:stroke-white"
+          onClick={() => setIsSettingsModalOpen(true)}
+        /> */}
+      </div>
+      <hr />
     </div>
   )
 }
+
+export default Navbar
