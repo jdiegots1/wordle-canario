a6e16335d25859b5cc75b6f83b63bc52648262a1
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-23 21:01:36 +0000

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,47 +1,32 @@
 import { ChartBarIcon, InformationCircleIcon, LightBulbIcon } from '@heroicons/react/outline'
-import { GAME_TITLE } from '../../constants/strings';
-import WCLOGO from '../../assets/WORDLE_CANARIO_LOGO.png';
-import twitter from '../../assets/x.png';
+import { GAME_TITLE } from '../../constants/strings'
+import WCLOGO from '../../assets/WORDLE_CANARIO_LOGO.png'
 
 type Props = {
-  setIsInfoModalOpen: (value: boolean) => void;
-  setIsStatsModalOpen: (value: boolean) => void;
-  setIsSettingsModalOpen: (value: boolean) => void;
-};
+  setIsInfoModalOpen: (value: boolean) => void
+  setIsStatsModalOpen: (value: boolean) => void
+  setIsSettingsModalOpen: (value: boolean) => void
+}
 
-export const Navbar = ({ setIsInfoModalOpen, setIsStatsModalOpen }: Props) => {
+const Navbar = ({ setIsInfoModalOpen, setIsStatsModalOpen, setIsSettingsModalOpen }: Props) => {
   return (
     <div>
-      {/* Logo Section */}
       <div className="flex w-40 mx-auto items-center mb-5 mt-8">
-        <img src={WCLOGO} alt="Wordle Canario Logo" />
+        <img src={WCLOGO} alt="" />
       </div>
-      
-      {/* Right Icons */}
       <div className="right-icons">
         <ChartBarIcon
           className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
           onClick={() => setIsStatsModalOpen(true)}
         />
-        <div className="flex h-6 w-6 mx-auto">
-          <a
-            title="Twitter"
-            href="https://x.com/wordlecanario"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={x} alt="X" />
-          </a>
-        </div>
-
-        {/* Uncomment if needed */}
         {/* <CogIcon
           className="h-6 w-6 cursor-pointer dark:stroke-white"
           onClick={() => setIsSettingsModalOpen(true)}
         /> */}
       </div>
-      
       <hr />
     </div>
-  );
-};
+  )
+}
+
+export default Navbar
