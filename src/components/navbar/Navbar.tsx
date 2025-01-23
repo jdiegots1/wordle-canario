87a0355a8eae4cf5e222072a1419618c229d5e61
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-23 20:48:04 +0000

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,38 +1,47 @@
-import { ChartBarIcon, InformationCircleIcon, LightBulbIcon } from '@heroicons/react/outline'
-import { GAME_TITLE } from '../../constants/strings'
-import WCLOGO from '../../assets/WORDLE_CANARIO_LOGO.png'
-import twitter from '../../assets/x.png'
+import { ChartBarIcon } from '@heroicons/react/outline';
+import { GAME_TITLE } from '../../constants/strings';
+import WCLOGO from '../../assets/WORDLE_CANARIO_LOGO.png';
+import twitter from '../../assets/x.png';
 
 type Props = {
-  setIsInfoModalOpen: (value: boolean) => void
-  setIsStatsModalOpen: (value: boolean) => void
-  setIsSettingsModalOpen: (value: boolean) => void
-}
+  setIsInfoModalOpen: (value: boolean) => void;
+  setIsStatsModalOpen: (value: boolean) => void;
+  setIsSettingsModalOpen: (value: boolean) => void;
+};
 
-        <div className="flex w-40 mx-auto items-center mb-5 mt-8">
-        <img
-          src={WCLOGO}
-          alt=""
-        />
-        </div>
-        <div className="right-icons">
-          <ChartBarIcon
-            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
-            onClick={() => setIsStatsModalOpen(true)}
-          />
-          <div className="flex h-6 w-6 mx-auto">
-            <a title="Twitter"
-              href="https://twitter.com/wordlecanario"><img
-              src={twitter}
-              alt="" /></a></div>
-            
-          {/* <CogIcon
-            className="h-6 w-6 cursor-pointer dark:stroke-white"
-            onClick={() => setIsSettingsModalOpen(true)}
-          /> */}
-        </div>
+export const Navbar = ({ setIsInfoModalOpen, setIsStatsModalOpen }: Props) => {
+  return (
+    <div>
+      {/* Logo Section */}
+      <div className="flex w-40 mx-auto items-center mb-5 mt-8">
+        <img src={WCLOGO} alt="Wordle Canario Logo" />
       </div>
-      <hr></hr>
+      
+      {/* Right Icons */}
+      <div className="right-icons">
+        <ChartBarIcon
+          className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+          onClick={() => setIsStatsModalOpen(true)}
+        />
+        <div className="flex h-6 w-6 mx-auto">
+          <a
+            title="Twitter"
+            href="https://twitter.com/wordlecanario"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="Twitter" />
+          </a>
+        </div>
+
+        {/* Uncomment if needed */}
+        {/* <CogIcon
+          className="h-6 w-6 cursor-pointer dark:stroke-white"
+          onClick={() => setIsSettingsModalOpen(true)}
+        /> */}
+      </div>
+      
+      <hr />
     </div>
-  )
-}
+  );
+};
