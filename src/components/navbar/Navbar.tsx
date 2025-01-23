a1e3549083534948ecc095af6b3c2ed94363d2cb
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-23 22:02:48 +0000

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,49 +1,40 @@
-import { ChartBarIcon, InformationCircleIcon, LightBulbIcon } from '@heroicons/react/outline';
-import { GAME_TITLE } from '../../constants/strings';
-import WCLOGO from '../../assets/WORDLE_CANARIO_LOGO.png';
-import x from '../../assets/x.png'; // Asegúrate de que la ruta sea correcta
+import { ChartBarIcon, InformationCircleIcon, LightBulbIcon } from '@heroicons/react/outline'
+import { GAME_TITLE } from '../../constants/strings'
+import WCLOGO from '../../assets/WORDLE_CANARIO_LOGO.png'
 
 type Props = {
-  setIsInfoModalOpen: (value: boolean) => void;
-  setIsStatsModalOpen: (value: boolean) => void;
-  setIsSettingsModalOpen: (value: boolean) => void;  // Asegurado aquí
-};
+  setIsInfoModalOpen: (value: boolean) => void
+  setIsStatsModalOpen: (value: boolean) => void
+  setIsSettingsModalOpen: (value: boolean) => void
+}
 
-export const Navbar = ({ setIsInfoModalOpen, setIsStatsModalOpen, setIsSettingsModalOpen }: Props) => {
+export const Navbar = ({ setIsInfoModalOpen, setIsStatsModalOpen }: Props) => {
   return (
-    <div>
-      {/* Logo Section */}
-      <div className="flex w-40 mx-auto items-center mb-5 mt-8">
-        <img src={WCLOGO} alt="Wordle Canario Logo" />
+    <div className="navbar">
+      <div className="navbar-banner px-5 py-2 bg-slate-200">
+        <p className="text-sm color-red bold mx-10 text-center">
+          CONSULTA LAS NOVEDADES DEL LUNES 16 DE MAYO{' '}<a href="https://i.imgur.com/6fuC5Yx.png" className="underline font-bold">AQUÍ</a>.
+        </p>
       </div>
-      
-      {/* Right Icons */}
-      <div className="right-icons">
-        <ChartBarIcon
-          className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
-          onClick={() => setIsStatsModalOpen(true)}
-        />
+      <div className="navbar-content px-5">
         <InformationCircleIcon
-          className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+          className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
           onClick={() => setIsInfoModalOpen(true)}
         />
-        <LightBulbIcon
-          className="h-6 w-6 cursor-pointer dark:stroke-white"
-          onClick={() => setIsSettingsModalOpen(true)}
+
+        <div className="flex w-40 mx-auto items-center mb-5 mt-8">
+        <img
+          src={WCLOGO}
+          alt=""
         />
-        <div className="flex h-6 w-6 mx-auto">
-          <a
-            title="Twitter"
-            href="https://twitter.com/wordlecanario"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={x} alt="Twitter" />
-          </a>
         </div>
+        <div className="right-icons">
+          <ChartBarIcon
+            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+            onClick={() => setIsStatsModalOpen(true)}
+          />
       </div>
-      
-      <hr />
+      <hr></hr>
     </div>
-  );
-};
+  )
+}
