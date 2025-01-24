b641bddf2c1d8e600d15b5d7f81cc74cc5f383c4
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-24 02:04:56 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -196,16 +196,16 @@ function App() {
       />
       {isBuyMeACoffeeVisible && (
   <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-    <div className="bg-white rounded-lg shadow-lg p-6 text-center max-w-lg w-full">
-      <p className="text-lg font-medium mb-4">
-        ¡Hola! 🙌 Soy Diego Tejera, creador del Wordle Canario.<br /> <br />
-        Me hace mucha ilusión que estés disfrutando del juego 🌟<br /> <br />
-        Si te lo pasas bien  jugando y te gustaría apoyar el proyecto de forma opcional... <br /> <br />
-        <a href="https://buymeacoffee.com/wordlecanario" target="_blank" className="text-indigo-600 font-semibold">¡Puedes invitarme a un cafecito ☕ haciendo clic aquí!</a><br /> <br />
-        Gracias por formar parte de esta aventura. <br /> <br />
-        Estoy trabajando en nuevas mejoras y juegos entretenidos para que sigas aquí :) <br /> <br />
-        Mientras tanto, ¡sigue disfrutando del Wordle Canario!
-      </p>
+  <div className="bg-white rounded-lg shadow-lg p-6 text-center max-w-lg w-full sm:max-w-md sm:p-4">
+    <p className="text-base sm:text-lg font-medium mb-4">
+      ¡Hola! 🙌 Soy Diego Tejera, creador del Wordle Canario.<br /> <br />
+      Me hace mucha ilusión que estés disfrutando del juego 🌟<br /> <br />
+      Si te lo pasas bien jugando y te gustaría apoyar el proyecto de forma opcional... <br /> <br />
+      <a href="https://buymeacoffee.com/wordlecanario" target="_blank" className="text-indigo-600 font-semibold">¡Puedes invitarme a un cafecito ☕ haciendo clic aquí!</a><br /> <br />
+      Gracias por formar parte de esta aventura. <br /> <br />
+      Estoy trabajando en nuevas mejoras y juegos entretenidos para que sigas aquí :) <br /> <br />
+      Mientras tanto, ¡sigue disfrutando del Wordle Canario!
+    </p>
       <button
         onClick={() => setIsBuyMeACoffeeVisible(false)}
         className="mt-2 px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
