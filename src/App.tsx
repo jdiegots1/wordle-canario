40dfae25d61496f9eb4651feb998d3a76ddf1b9a
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-24 01:26:29 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -195,11 +195,11 @@ function App() {
   <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
     <div className="bg-white rounded-lg shadow-lg p-6 text-center max-w-sm w-full">
       <p className="text-lg font-medium mb-4">
-        ¡Hola! 🙌<br />
-        Soy Diego Tejera, creador del Wordle Canario <br />
-        Me hace mucha ilusión que estés jugando 🌟<br />
-        Si te lo pasas bien y quieres apoyar el proyecto de manera opcional, puedes invitarme a un cafecito haciendo <a href="https://buymeacoffee.com/wordlecanario" target="_blank" className="text-indigo-600 font-semibold">click aquí</a>.<br />
-        ¡Gracias por ser parte de esta aventura! Estoy trabajando en nuevos y entretenidos juegos para ti. <br />
+        ¡Hola! 🙌<br /> <br />
+        Soy Diego Tejera, creador del Wordle Canario <br /> <br />
+        Me hace mucha ilusión que estés jugando 🌟<br /> <br />
+        Si te lo pasas bien y quieres apoyar el proyecto de manera opcional, puedes invitarme a un cafecito haciendo <a href="https://buymeacoffee.com/wordlecanario" target="_blank" className="text-indigo-600 font-semibold">click aquí</a>.<br /> <br />
+        ¡Gracias por ser parte de esta aventura! Estoy trabajando en nuevos y entretenidos juegos para ti. <br /> <br />
         Mientras tanto, ¡sigue disfrutando del Wordle Canario!
       </p>
       <button
