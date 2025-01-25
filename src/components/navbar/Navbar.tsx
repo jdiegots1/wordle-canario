a055f9d8db5c23528c2ffa5398159712758697bf
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-25 17:25:31 +0000

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,15 +15,15 @@ export const Navbar = ({
 }: Props) => {
   return (
     <div className="navbar">
-      {/* Eliminamos la sección de novedades del 16 de mayo */}
-      <div className="navbar-banner px-5 py-2 bg-slate-200">
-        <p className="text-sm color-red bold mx-10 text-center">
-          ¡Si te gusta el Wordle Canario, puedes invitarme a un leche y leche (o a un barraquito ☺️ ) y ayudarme a mantenerlo vivo! ☕{' '}
-          <a href="https://www.paypal.com/paypalme/wordlecanario" className="underline font-bold text-lg text-green-600">
-            ¡Hazlo pulsando aquí y echa una manita!
-          </a>
-        </p>
-      </div>
+  <div className="navbar-banner px-5 py-2 bg-slate-200">
+    <p className="text-sm color-red bold mx-10 text-center">
+      ¿Te gusta el Wordle Canario? ¡Puedes apoyarme para mantenerlo en marcha! 😊{' '}
+      <a href="https://www.paypal.com/paypalme/wordlecanario" className="underline font-bold text-lg text-green-600">
+        ¡Hazlo aquí!
+      </a>
+    </p>
+  </div>
+</div>
       <div className="navbar-content px-5">
         <div className="flex items-center cursor-pointer" onClick={() => setIsInfoModalOpen(true)}>
           <InformationCircleIcon
