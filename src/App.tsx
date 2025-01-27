bca8bdb57412fd2302ff83bd8dda77b834910526
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-01-27 01:13:53 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Keyboard } from './components/keyboard/Keyboard'
 import { InfoModal } from './components/modals/InfoModal'
 import { StatsModal } from './components/modals/StatsModal'
 import { AboutModal } from './components/modals/AboutModal'
+import { QuestionModal } from './components/modals/QuestionModal'
 import { AnterioresModal } from './components/modals/AnterioresModal'
 import {
   WIN_MESSAGES,
@@ -44,6 +45,12 @@ function App() {
     useAlert()
   const [currentGuess, setCurrentGuess] = useState('')
   const [isGameWon, setIsGameWon] = useState(false)
+  const [isQuestionModalOpen, setIsQuestionModalOpen] = useState(false);
+
+const handleStartGameClick = () => {
+  setIsWelcomeScreenOpen(false); // Ocultar la pantalla de bienvenida
+  setIsQuestionModalOpen(true);  // Mostrar el modal de la pregunta
+};
   const [isWelcomeScreenOpen, setIsWelcomeScreenOpen] = useState(true)
   const [isInfoModalOpen, setIsInfoModalOpen] = useState(false)
   const [fromWelcomeScreen, setFromWelcomeScreen] = useState(false)
@@ -209,65 +216,49 @@ function App() {
           src="/WORDLE_CANARIO_LOGO.png" 
           alt="Logo de Wordle Canario"
           className="w-24 mx-auto"
-          // Ajusta el tamaño según sea necesario
         />
       </div>
-      {/* Párrafo 1: Bienvenida */}
+      {/* Párrafos de bienvenida */}
       <p className="text-xs sm:text-sm font-medium mb-4">
         <strong>¡Bienvenido al Wordle Canario! 🌴</strong>
       </p>
-      
-      {/* Párrafo 2: Explicación corta del juego */}
       <p className="text-xs sm:text-sm font-medium mb-4">
         El juego inspirado en el famoso Wordle, pero con <strong>nuestro toque</strong>.
       </p>
-      
-      {/* Párrafo 3: Mantenimiento y nuevos juegos */}
       <p className="text-xs sm:text-sm font-medium mb-4">
-        El juego está en constante mantenimiento para seguir mejorando tu experiencia. <strong>Estoy trabajando en nuevos juegos</strong> para que puedas disfrutar aún más, y lo mejor de todo es que <strong><u>puedes jugar sin publicidad</u></strong>. <strong>Tu apoyo es esencial</strong> para poder seguir ofreciendo juegos gratuitos y de calidad.
+        El juego está en constante mantenimiento... <strong>puedes jugar sin publicidad</strong>.
+      </p>
+      <p className="text-xs sm:text-sm font-medium mb-4">
+        Si te gusta el juego, ¡puedes apoyarme donando en <a href="https://www.paypal.me/wordlecanario" target="_blank" className="underline text-blue-500">www.paypal.me/wordlecanario</a>!
       </p>
       
-      {/* Párrafo 4: Petición de donaciones */}
-      <p className="text-xs sm:text-sm font-medium mb-4">
-        Así que, si te gusta el juego, ¡puedes apoyarme donando en <a href="https://www.paypal.me/wordlecanario" target="_blank" className="underline text-blue-500">www.paypal.me/wordlecanario</a>!
-      </p>
-      
-      <div className="grid grid-cols-3 gap-4 items-center">
-        <div
-  onClick={() => {
-    setIsInfoModalOpen(true)
-    setIsWelcomeScreenOpen(false)
-    setFromWelcomeScreen(true)
-  }}
-  className="flex flex-col items-center cursor-pointer text-indigo-600 hover:text-indigo-700"
->
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-8 w-8">
-    <path fillRule="evenodd" d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z" clipRule="evenodd" />
-  </svg>
-  <span className="text-sm mt-2">Cómo jugar</span>
-</div>
-         <div
-          onClick={() => {
-            setAccessedFromBlock(true);
-            setIsStatsModalOpen(true);
-            setIsWelcomeScreenOpen(false);
-          }}
-          className="flex flex-col items-center cursor-pointer text-green-600 hover:text-green-700"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-8 w-8">
-            <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75ZM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 0 1-1.875-1.875V8.625ZM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 0 1 3 19.875v-6.75Z" />
-          </svg>
-          <span className="text-sm mt-2">Mis estadísticas</span>
-        </div>
-         <div
-          onClick={() => setIsWelcomeScreenOpen(false)}
-          className="flex flex-col items-center cursor-pointer text-blue-600 hover:text-blue-700"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-8 w-8">
-            <path d="M11.25 5.337c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.036 1.007-1.875 2.25-1.875S15 2.34 15 3.375c0 .369-.128.713-.349 1.003-.215.283-.401.604-.401.959 0 .332.278.598.61.578 1.91-.114 3.79-.342 5.632-.676a.75.75 0 0 1 .878.645 49.17 49.17 0 0 1 .376 5.452.657.657 0 0 1-.66.664c-.354 0-.675-.186-.958-.401a1.647 1.647 0 0 0-1.003-.349c-1.035 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401.31 0 .557.262.534.571a48.774 48.774 0 0 1-.595 4.845.75.75 0 0 1-.61.61c-1.82.317-3.673.533-5.555.642a.58.58 0 0 1-.611-.581c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.035-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959a.641.641 0 0 1-.658.643 49.118 49.118 0 0 1-4.708-.36.75.75 0 0 1-.645-.878c.293-1.614.504-3.257.629-4.924A.53.53 0 0 0 5.337 15c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.036 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.369 0 .713.128 1.003.349.283.215.604.401.959.401a.656.656 0 0 0 .659-.663 47.703 47.703 0 0 0-.31-4.82.75.75 0 0 1 .83-.832c1.343.155 2.703.254 4.077.294a.64.64 0 0 0 .657-.642Z" />
-          </svg>
-          <span className="text-sm mt-2">Jugar al Wordle Canario</span>
-        </div>
+      {/* Botón para mostrar la pregunta */}
+      <div
+        onClick={handleStartGameClick}
+        className="flex flex-col items-center cursor-pointer text-blue-600 hover:text-blue-700"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-8 w-8">
+          <path d="M11.25 5.337c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.036 1.007-1.875 2.25-1.875S15 2.34 15 3.375c0 .369-.128.713-.349 1.003-.215.283-.401.604-.401.959 0 .332.278.598.61.578 1.91-.114 3.79-.342 5.632-.676a.75.75 0 0 1 .878.645 49.17 49.17 0 0 1 .376 5.452.657.657 0 0 1-.66.664c-.354 0-.675-.186-.958-.401a1.647 1.647 0 0 0-1.003-.349c-1.035 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401.31 0 .557.262.534.571a48.774 48.774 0 0 1-.595 4.845.75.75 0 0 1-.61.61c-1.82.317-3.673.533-5.555.642a.58.58 0 0 1-.611-.581c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.035-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959a.641.641 0 0 1-.658.643 49.118 49.118 0 0 1-4.708-.36.75.75 0 0 1-.645-.878c.293-1.614.504-3.257.629-4.924A.53.53 0 0 0 5.337 15c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.036 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.369 0 .713.128 1.003.349.283.215.604.401.959.401a.656.656 0 0 0 .659-.663 47.703 47.703 0 0 0-.31-4.82.75.75 0 0 1 .83-.832c1.343.155 2.703.254 4.077.294a.64.64 0 0 0 .657-.642Z" />
+        </svg>
+        <span className="text-sm mt-2">Jugar al Wordle Canario</span>
+      </div>
+    </div>
+  </div>
+)}
+
+{/* Modal de la pregunta */}
+{isQuestionModalOpen && (
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="bg-white rounded-lg shadow-lg p-6 text-center max-w-lg w-full sm:max-w-md sm:p-4">
+      <p className="text-lg font-bold">Oye... Tengo una pregunta.</p>
+      <p className="text-md mt-4">¿Sabrías decirme cómo sigue la frase?</p>
+      <p className="text-md font-semibold mt-2">"Abre los ojos y..."</p>
+      <p className="text-sm mt-4 text-gray-600">Respuesta: <strong>desparrama la vista</strong></p>
+      <div
+        onClick={() => setIsQuestionModalOpen(false)}
+        className="mt-6 px-4 py-2 bg-blue-600 text-white rounded-lg cursor-pointer"
+      >
+        ¡Entendido!
       </div>
     </div>
   </div>
