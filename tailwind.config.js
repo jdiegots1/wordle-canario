074f9ca06e10a54cfac8ba256d79c0c5f804e57c
jdiegots1 <99126828+jdiegots1@users.noreply.github.com>
2025-10-27 22:32:15 +0000

    Update Tailwind CSS configuration for content and safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './public/index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
   darkMode: 'class',
   theme: {
     extend: {},
   },
+  safelist: [
+    { pattern: /(bg|text|border)-(gray|green|yellow|red|blue|indigo)-(100|200|300|400|500|600|700|800|900)/ }
+  ],
   plugins: [],
 }
